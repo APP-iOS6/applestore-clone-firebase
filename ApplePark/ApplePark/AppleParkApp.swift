7cdee2665887f7da40922c23517dff599d605939
Bbell <428bbell@gmail.com>
2024-10-07 14:23:54 +0900

    [add]: model

---
+++ ApplePark/ApplePark/AppleParkApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleParkApp.swift
+//  ApplePark
+//
+//  Created by 박준영 on 10/7/24.
+//
+
+import SwiftUI
+
+@main
+struct AppleParkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
