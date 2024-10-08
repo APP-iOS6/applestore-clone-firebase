a196be72860fd288ab65c4d4c0163039d3c208ca
Soomin Kim <140694831+sumchive@users.noreply.github.com>
2024-10-08 10:58:44 +0900

    [feat] : 로그아웃

--- ApplePark/ApplePark/ProductAddView.swift
+++ ApplePark/ApplePark/ProductAddView.swift
@@ -11,20 +11,16 @@ struct ProductAddView: View {
     let gridItem = Array(repeating: GridItem(.flexible()), count: 2)
     @EnvironmentObject var authManager: AuthManager
     @EnvironmentObject private var itemStore: ItemStore
+    @State private var isLogout: Bool = false
     
     var body: some View {
         NavigationStack {
-            if itemStore.items.isEmpty {
-                ProgressView()
-                    .onAppear {
-                        Task {
-                            await itemStore.loadItems(userID: authManager.userID)
-                        }
-                    }
+            if isLogout {
+                LoginView()
             } else {
                 VStack {
                     ScrollView {
-                        LazyVGrid(columns: gridItem ) {
+                        LazyVGrid(columns: gridItem) {
                             ForEach(itemStore.items, id: \.itemId) { item in
                                 Text("\(item.name)")
                                     .frame(maxWidth: .infinity)
@@ -34,35 +30,50 @@ struct ProductAddView: View {
                                             .fill(.orange.gradient)
                                     )
                             }
-                           
+                        }
+                        .onAppear {
+                            Task {
+                                await itemStore.loadItems(userID: authManager.userID)
+                            }
+                        }
+                    }
+                    .padding(.horizontal, 20)
+                    .navigationTitle("Product Add View")
+                    .toolbar {
+                        ToolbarItem(placement: .topBarTrailing) {
+                            Button {
+                                Task {
+                                    await authManager.itemStore.addItem(Item(name: "name데이터",
+                                                                             category: "name데이터",
+                                                                             price: 100,
+                                                                             description: "name데이터",
+                                                                             stockQuantity: 1200,
+                                                                             imageURL: "name데이터",
+                                                                             color: "name데이터",
+                                                                             isAvailable: true), userID: authManager.userID)
+                                    await itemStore.loadItems(userID: authManager.userID)
+                                }
+                            } label: {
+                                Image(systemName: "plus")
+                            }
+                            
+                            
                         }
                     }
                 }
-                .padding(.horizontal, 20)
-                .navigationTitle("Product Add View")
-                .toolbar {
-                    ToolbarItem (placement: .topBarTrailing) {
-                        Button {
-                            Task {
-                                await authManager.itemStore.addItem(Item(name: "name데이터",
-                                                                         category: "name데이터",
-                                                                         price: 100,
-                                                                         description: "name데이터",
-                                                                         stockQuantity: 1200,
-                                                                         imageURL: "name데이터",
-                                                                         color: "name데이터",
-                                                                         isAvailable: true), userID: authManager.userID)
-                                await itemStore.loadItems(userID: authManager.userID)
-                            }
-                        } label: {
-                            Image(systemName: "plus")
-                        }
-                    }
+                
+                Button {
+                    authManager.signOut()
+                    isLogout = true
+                } label: {
+                    Image(systemName: "rectangle.portrait.and.arrow.forward")
                 }
             }
         }
     }
 }
+
+
 
 #Preview {
     NavigationStack {
